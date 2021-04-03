32bbc4a547efcfcdeb69a33107a02fd356a988a4
Dank <76633816+danktheskid@users.noreply.github.com>
2021-04-03 15:30:17 -0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,3 @@
+local target = game:GetService("Players").YOURUSERNAME
+
+target.Character:FindFirstChild("Head"):FindFirstChild("face").Texture = "http://www.roblox.com/asset/?id=FACETEXTUREIDHERE"
